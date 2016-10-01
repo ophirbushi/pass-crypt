c6ad7c4d36e92797a7a10295eace7a186492b8d7
ophirbushi <ophirbushi@gmail.com>
2016-10-01 22:34:12 +0300

    fix

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
     res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(port);
