cdf7d8e102a4a7a96bdcfe0acf7836e425e98ef7
ophirbushi <ophirbushi@gmail.com>
2016-10-02 13:53:16 +0300

    ejs

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.render('index.ejs', {text: 'ejs rendered text'});
 });
 
 app.listen(port);
